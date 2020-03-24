a8874be9e402a22c61b52aea719c7d5f27e038f9
Naruterador <103258937@qq.com>
2020-03-24 23:21:02 +0800

    update

--- datastruct/linear_table/queue/exercise/event/event.c
+++ datastruct/linear_table/queue/exercise/event/event.c
@@ -28,7 +28,6 @@ struct eventlist
     //用户事件
     struct Event en;  
     struct eventlist * pNext;
-    int trigger;
 
 };
 
@@ -84,63 +83,32 @@ void random_gen(int *,int *);
 
 
 //初始化一天的模拟
-void OpenForDay();
+void OpenForDay(struct eventlist * enlist,struct linequeue *,int * Totaltime);
 //模拟用户到达事件
-void CustomerArrived();
+void CustomerArrived(int * countcustom,struct eventlist * ,struct eventlist *,struct linequeue *,int *,int *,int *);
 //模拟用户离开事件
-void CustomerDeparture();
+void CustomerDeparture(struct eventlist * ,struct linequeue *,struct eventlist *,int * Totaltime);
 //模拟银行的事务
-void Blank_Simulation();
+void Blank_Simulation(struct eventlist *,struct linequeue *,int *,int *,int *,int *,int *);
 
 
 
 int main(void)
 {
     struct eventlist * pHead = NULL;
-    struct eventlist * getp;
-    
-    struct linequeue queue[4];
-    struct queueNode test;
-    int length;
-
-    int a;
-    int b;
-    random_gen(&a,&b);
-    printf("%d,%d\n",a,b);
-    
-   
-    //test = delete_linequeue(&queue);
-    //test = delete_linequeue(&queue);
-    /*
     pHead = init_eventlist();
-    insert_eventlist(pHead,0,0);
-    insert_eventlist(pHead,1,1);
-    insert_eventlist(pHead,5,3);
-    insert_eventlist(pHead,6,10);
-    traverse_linked(pHead);
-    getp= delete_eventFirst(pHead);
-    printf("%d,%d\n",getp->en.enType,getp->en.occurtime);
-    pHead = eventChangeFrist(pHead);
-    getp = delete_eventFirst(pHead);
-    printf("%d,%d\n",getp->en.enType,getp->en.occurtime);     
-    pHead = eventChangeFrist(pHead);
-    getp = delete_eventFirst(pHead);
-    printf("%d,%d\n",getp->en.enType,getp->en.occurtime);   
-    
-    struct queueNode test;
-    init_linequeue(&queue);
-    insert_linequeue(&queue,0,0);
-    insert_linequeue(&queue,1,1);
-    insert_linequeue(&queue,2,2);
-    insert_linequeue(&queue,3,3);
-    test = delete_linequeue(&queue);
-    test = delete_linequeue(&queue);
-    test = delete_linequeue(&queue);
-    printf("%d,%d\n",test.arrivaltime,test.duration);
-    */
-
-    
-
+    struct linequeue queue[4];
+    //总时间
+    int Totaltime = 0;
+    //总人数;
+    int countCustom = 0;
+    //持续时间
+    int durtime;
+    //每个用户到达的间隔时间
+    int intertime;
+    //关门时间(单位分钟)
+    int closetime = 480;
+    Blank_Simulation(pHead,queue,&Totaltime,&durtime,&intertime,&closetime,&countCustom);
     return 0;
 
 }
@@ -156,42 +124,109 @@ struct eventlist * init_eventlist()
         exit(1);
     }   
     pHead->pNext = NULL;
-    pHead->trigger = -1;
-
     return pHead;
 }
 
-//事件插入（插入事件链表）
+//事件插入（插入事件链表）要求，每次插入时按照时间先后顺序插入
 void insert_eventlist(struct eventlist * enlist,int oct,int type)
 {   
-    
+   
     struct eventlist * pTail = enlist;
-    struct eventlist * pNew = (struct eventlist *)malloc(sizeof(struct eventlist));
-    if(-1 == pTail->trigger)
+    struct eventlist * temp;
+    if(is_lempty(pTail))
     {
-        pTail->en.enType = type;
-        pTail->en.occurtime = oct;
-        pTail->trigger = 1;
-    }
-    else{
+        struct eventlist * pNew = (struct eventlist *)malloc(sizeof(struct eventlist));
         if(NULL == pNew)
         {
             printf("Can not assign the space for the pNew");
             exit(1);
 
         }
-        while(NULL != pTail->pNext)
-        {
-            pTail = pTail->pNext;
-        }
-        
         pNew->en.enType = type;
-        pNew->en.occurtime = oct;
+        pNew->en.occurtime = oct; 
         pTail->pNext = pNew;
         pNew->pNext = NULL;
-        pTail = pNew;
     }
-    
+    else{
+        int i = 0;
+        struct eventlist * p = pTail->pNext;
+        struct eventlist * temp;
+        struct eventlist * pNew = (struct eventlist *)malloc(sizeof(struct eventlist));
+        if(NULL == pNew)
+        {
+            printf("Can not assign the space for the pNew");
+            exit(1);
+
+        }
+        //通过判断链表的occurtime大小，进行插入
+        while(oct > p->en.occurtime && NULL != p->pNext)
+        {
+            temp = p;
+            p = p->pNext;
+            i++;
+        }
+        //当链表只有一个节点时，对插入的操作
+        if(0 == i && NULL == p->pNext)
+        {
+            if(oct >= p->en.occurtime)
+            {
+                pNew->en.enType = type;
+                pNew->en.occurtime = oct;
+                pNew->pNext = NULL;
+                p->pNext = pNew;
+            }
+            if(oct < p->en.occurtime)
+            {
+                pNew->en.enType = type;
+                pNew->en.occurtime = oct;
+                pNew->pNext = p;
+                pTail->pNext = pNew;
+            }
+        }
+        //如果插入的节点比首节点小而且节点数大于1，就按照一下方式处理
+        else if(0 == i && NULL != p->pNext)
+        {
+             if(oct >= p->en.occurtime)
+            {
+                pNew->en.enType = type;
+                pNew->en.occurtime = oct;
+                pNew->pNext = p->pNext;
+                p->pNext = pNew;
+            }
+            if(oct < p->en.occurtime)
+            {
+                pNew->en.enType = type;
+                pNew->en.occurtime = oct;
+                pNew->pNext = p;
+                pTail->pNext = pNew;
+            }
+
+        }
+        //如果链表节点数大于1,对插入的处理
+        else if(0 != i && NULL == p->pNext)
+        {
+                if(oct >= p->en.occurtime)
+                {
+                    pNew->en.enType = type;
+                    pNew->en.occurtime = oct;
+                    pNew->pNext  = NULL;
+                    p->pNext = pNew;
+                }
+                if(oct < p->en.occurtime)
+                {
+                    pNew->en.enType = type;
+                    pNew->en.occurtime = oct;
+                    pNew->pNext = p;
+                    temp->pNext = pNew;
+                }
+        }else{
+                    pNew->en.enType = type;
+                    pNew->en.occurtime = oct;
+                    pNew->pNext = p;
+                    temp->pNext = pNew;
+        }
+        
+    }
 }
 
 //取出当前事件链表的第一个节点
@@ -202,19 +237,15 @@ struct eventlist * delete_eventFirst(struct eventlist * enlist)
         printf("ERROR LINKED LIST IS EMPTY!");
         exit(1);
     }
+    
     struct eventlist * pHead;
-    pHead = enlist;    
-    return pHead;
+    struct eventlist * pFirst;
+    pHead = enlist;
+    pFirst = pHead->pNext;
+    pHead->pNext = pHead->pNext->pNext;        
+    return pFirst;
 }
-//删除原来链表元素的第一个节点，并将头节点向后移动
-struct eventlist * eventChangeFrist(struct eventlist * enlist)
-{
-    struct eventlist * nPhead;
-    nPhead = enlist->pNext;
-    free(enlist);
-    enlist = NULL;
-    return nPhead;
-}
+
 
 //检查链表是否为空
 bool is_lempty(struct eventlist * enlist)
@@ -230,10 +261,10 @@ void traverse_linked(struct eventlist * enlist)
     struct eventlist * p = enlist;
     while( NULL != p->pNext)
     {
-        printf("%d,%d\n",p->en.enType,p->en.occurtime);
+        
         p = p->pNext;
+        printf("%d,%d\n",p->en.occurtime,p->en.enType);
     }
-    printf("%d,%d\n",p->en.enType,p->en.occurtime);
 }
 
 
@@ -264,7 +295,6 @@ void insert_linequeue(struct linequeue * qu,int arrivaltime,int duratime)
     qu->custom[qu->rear].arrivaltime = arrivaltime;
     qu->custom[qu->rear].duration = duratime;
     qu->rear++;
-
 }
 
 //删除队列元素
@@ -306,7 +336,7 @@ int length(struct linequeue * qu)
 {
     int length;
     length = qu->rear - qu->front;
-    return length - 1;
+    return length;
 }
 
 
@@ -341,4 +371,86 @@ void random_gen(int * durtime,int * intertime)
     srand(time(NULL));
     *durtime = rand() % 30;
     *intertime = rand() % 5;
+}
+
+
+//初始化一天的银行事件
+void OpenForDay(struct eventlist * enlist,struct linequeue * qu ,int * Totaltime)
+{
+    //模拟当天的第一个客户进入银行
+    insert_eventlist(enlist,0,0);
+
+    //初始化4个队列
+    init_multiplequeue(qu);
+}
+
+
+//处理增加客户的函数
+void CustomerArrived(int * countcustom,struct eventlist * enlist,struct eventlist * Node,struct linequeue *qu,int * durtime,int * intertime,int * Closetime)
+{
+    int t = 0;
+    //找到最小的队列
+    int i = 0;
+    //随机生成上一个用户办理业务持续时间和下一个用户到达的间隔时间
+    random_gen(durtime,intertime);
+    //下一个用户的到达时间为上一用户的到家时间+间隔时间    
+    t = Node->en.occurtime + *intertime;
+    //每多一个用户增加1
+    *countcustom = *countcustom + 1;
+    //假设银行还在营业,就让用户进入事件
+    if(t < *Closetime)
+        insert_eventlist(enlist,t,0);
+    //找到最小的队列并将上个用户开始业务的时间和持续时间放进队列，开始排队
+    i = Minimumqueue(qu);
+    //将上一个用户放入人数最少的队列
+    insert_linequeue(&qu[i],Node->en.occurtime,*durtime);    
+    printf("第%d用户进入银行队列%d排队，排队开始时间为%d\n",*countcustom,i + 1,t);
+    //当前队列如果只有1个人排队，那就直接生成上一个用户的出队列事件，并压入事件链表
+    if(length(&qu[i]) == 1)
+    {
+        insert_eventlist(enlist,Node->en.occurtime + *durtime,i);
+    }
+}
+
+
+void CustomerDeparture(struct eventlist * enlist ,struct linequeue * qu,struct eventlist * Node,int * Totaltime)
+{
+    struct queueNode leaveclient;
+    struct queueNode lineNextclient;
+    int i = 0;
+    //获取当前离开事件的队列
+    i = Node->en.enType;
+    //用户离开队列
+    leaveclient = delete_linequeue(&qu[i]);
+    //计算这个用户在银行中逗留的时间
+    *Totaltime = *Totaltime + (Node->en.occurtime - leaveclient.arrivaltime);
+    //printf("第%d分钟到银行的用户办理业务完成离开银行，离开时间为时间为%d\n",leaveclient.arrivaltime,Node->en.occurtime);
+    //如果这个队列不为空，则把现在队列头部的用户添加离开事件
+    if(!is_qempty(&qu[i]))
+    {   
+        //获得队列i的新排头
+       lineNextclient =  GetHead(&qu[i]);
+       //这个用户的离开事件插入链表
+       insert_eventlist(enlist,Node->en.occurtime + lineNextclient.duration,i);
+
+    }
+}
+
+void Blank_Simulation(struct eventlist * enlist,struct linequeue * qu,int * Totaltime,int * durtime,int * intertime,int * Closetime,int * countcustom)
+{
+    double averagetime;
+    struct  eventlist * p;
+    //初始化这一天
+    OpenForDay(enlist,qu,Totaltime);
+    while(!is_lempty(enlist))
+    {
+       p = delete_eventFirst(enlist);
+       if(p->en.enType == 0)
+            CustomerArrived(countcustom,enlist,p,qu,durtime,intertime,Closetime);
+        else
+            CustomerDeparture(enlist,qu,p,Totaltime);
+    }
+    printf("用户总共逗留的时间为%d,一共接待了%d个用户\n",*Totaltime,*countcustom);
+    averagetime = *Totaltime / *countcustom;
+    printf("用户平均逗留时间为%f",averagetime);
 }
