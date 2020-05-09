55e442fe4b0e33f58501e71395f5b07297597372
Naruterador <103258937@qq.com>
2020-05-10 00:53:52 +0800

    update

--- datastruct/nolinear_table/tree/BinaryTree/ParentsDescribing/ParentsDe.c
+++ datastruct/nolinear_table/tree/BinaryTree/ParentsDescribing/ParentsDe.c
@@ -9,20 +9,44 @@
 #include "queue.h"
 
 
-
 //构造空树T
 void InitTree(BiTree * T);
 //构造树T
 void CreateTree(BiTree * T);
 //判断树是否为空
 bool isTempty(BiTree T);
+//返回树的深度算法1
+int TreeDepth1(BiTree T);
+//返回树的深度算法2
+int TreeDepth2(BiTree T);
+//如果树T存在,返回T的根
+char Root(BiTree);
+//返回树中第i个结点的值
+char Value(BiTree,int);
+//改变树中某一结点的值
+void Assign(BiTree,char,char);
+//返回树中结点的父结点的值
+char Parent(BiTree,char);
+//返回二叉树中某个结点的左孩子
+char LeftChild(BiTree,char);
+//返回二叉树中某个结点的右兄弟
+char RightSibling(BiTree,char);
+//输出树
+void Print(BiTree);
+
 
 
 int main(void)
 {
+    int depth;
+    char rs;
     BiTree T;
     InitTree(&T);
     CreateTree(&T);
+    //depth = TreeDepth2(T);
+    //rs = RightSibling(T,'A');
+    //printf("%c",rs);
+    Print(T);
     return 0;
 
 
@@ -100,4 +124,149 @@ void CreateTree(BiTree * T)
 
 }
 
+bool isTempty(BiTree T)
+{
+
+    if(0 == T->n)
+        return true;
+    return false;
+}
+
+
+int TreeDepth1(BiTree T)
+{
+
+    int j = 0;
+
+    while(T->n >= pow(2,j))
+        j++;
+
+    return j;
+}
+
+int TreeDepth2(BiTree T)
+{
+    int k;
+    int m;
+    int def;
+    int max = 0;
+    
+    if(T)
+    {
+        for(k = 0;k < T->n;k++)
+        {
+            def = 1;
+            m = T->nodes[k].parent;
+            while(m != -1)
+            {
+                m = T->nodes[m].parent;
+                def++;
+            }
+            if(max < def)
+                max = def;
+        }
+
+        return max;
+    }
+    
+    return 0;
+
+}
+
+
+
+char Root(BiTree T)
+{
+    int i;
+    for(i = 0;i < T->n;i++)
+        if(T->nodes[i].parent < 0)
+            return T->nodes[i].data;
+    
+    return ' ';
+
+}
+
+
+char Value(BiTree T,int i)
+{
+    if(i < T->n)
+        return T->nodes[i].data;
+    
+    return ' ';
+}
+
+
+void Assign(BiTree T,char cur,char value)
+{
+    
+    int i;
+    for(i = 0;i < T->n;i++)
+    {
+        if(cur == T->nodes[i].data)
+            T->nodes[i].data = value;
+    }
+
+}
+
+
+char Parent(BiTree T,char cur)
+{
+    char p;
+    int i;
+    for(i = 0;i < T->n;i++)
+    {
+       if(cur == T->nodes[i].data) 
+            return T->nodes[T->nodes[i].parent].data;
+    }
+    return ' ';
+}
+
+
+char LeftChild(BiTree T,char cur)
+{
+    int i;
+    int j;
+    for(i = 0;i < T->n;i++)
+    {
+        if(cur == T->nodes[i].data)
+           break; 
+    }
+
+    for(j = i + 1;j < T->n;j ++)
+       if(T->nodes[j].parent == i)
+            return T->nodes[j].data;
+
+    return ' ';
+}
+
+char RightSibling(BiTree T,char cur)
+{
+    int i;
+    int j = 0;
+    
+    for(i = 0;i < T->n;i++)
+        if(cur == T->nodes[i].data)
+            break;
+    for(j = i + 1;j < T->n;j++)
+    {  
+        if(T->nodes[j].parent == T->nodes[i].parent)
+            return T->nodes[j].data;
+    }
+    return ' ';
+}
+
+void Print(BiTree T)
+{
+    printf("树的结点总数为%d",T->n);
+    printf("\n");
+    printf("树的根结点为:");
+    int i;
+    for(i = 0;i < T->n;i++)
+    {
+        printf("%c",Value(T,i));
+        if(T->nodes[i].parent >= 0)
+            printf("父节点为%c",T->nodes[T->nodes[i].parent].data);
+        printf("\n");
+    }
+}
 
