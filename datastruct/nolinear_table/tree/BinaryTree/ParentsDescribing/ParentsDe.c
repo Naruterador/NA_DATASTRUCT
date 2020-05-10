b6add5153dc4444d1273925cdaed726a1d89326e
Naruterador <103258937@qq.com>
2020-05-10 23:07:22 +0800

    update

--- datastruct/nolinear_table/tree/BinaryTree/ParentsDescribing/ParentsDe.c
+++ datastruct/nolinear_table/tree/BinaryTree/ParentsDescribing/ParentsDe.c
@@ -10,9 +10,9 @@
 
 
 //构造空树T
-void InitTree(BiTree * T);
+void InitTree(BiTree T);
 //构造树T
-void CreateTree(BiTree * T);
+void CreateTree(BiTree T);
 //判断树是否为空
 bool isTempty(BiTree T);
 //返回树的深度算法1
@@ -34,38 +34,44 @@ char RightSibling(BiTree,char);
 //输出树
 void Print(BiTree);
 //插入c为T中p结点的第i棵子树
-void InsertChild(BiTree * T,char p,int i,BiTree c);
+void InsertChild(BiTree T,char p,int i,BiTree c);
+//删除T中结点为p的第i棵子树
+void DeleteChild(BiTree T,char p,int i);
+//删除用标记数组
+int deleted[MAX_TREE_SIZE + 1];
 
 
 int main(void)
 {
     int depth;
     char rs;
-    BiTree T;
-    BiTree C;
+    struct Tree T;
+    struct Tree C;
+
     InitTree(&T);
+    InitTree(&C);
     CreateTree(&T);
-    //InitTree(&C);
-    //CreateTree(&C);
-    //InsertChild(&T,'R',3,C);
-    Print(T);
+    CreateTree(&C);
+    InsertChild(&T,'R',3,&C);
+    DeleteChild(&T,'R',3);
+    Print(&T);
     return 0;
 
 
 }
 
 
-void InitTree(BiTree * T)
+void InitTree(BiTree T)
 {
 
-    (*T)->n = 0; //结点树为0
+    T->n = 0; //结点树为0
 
 }
 
 
 
 
-void CreateTree(BiTree * T)
+void CreateTree(BiTree T)
 {
     
     int i = 1;  //顺序结构的非根结点的下标
@@ -81,15 +87,15 @@ void CreateTree(BiTree * T)
     
     printf("请输入根结点的值:");
     char c[MAX_TREE_SIZE];
-    scanf("%c%*c",&(*T)->nodes[0].data);
+    scanf("%c%*c",&T->nodes[0].data);
 
-   if((*T)->nodes[0].data != ' ')
+   if(T->nodes[0].data != ' ')
    {
-        (*T)->nodes[0].parent = -1;
-        (*T)->r = 0;
+        T->nodes[0].parent = -1;
+        T->r = 0;
 
         q1.num = 0;
-        q1.tdata = (*T)->nodes[0].data;
+        q1.tdata = T->nodes[0].data;
         
         insert_queue(&queue,q1);
         
@@ -103,8 +109,8 @@ void CreateTree(BiTree * T)
             {
                 if(c[j] == '\n')
                     break;
-                (*T)->nodes[i].data = c[j];
-                (*T)->nodes[i].parent = q1.num;
+                T->nodes[i].data = c[j];
+                T->nodes[i].parent = q1.num;
                 q2.num = i;
                 q2.tdata = c[j];
                 insert_queue(&queue,q2);
@@ -118,11 +124,11 @@ void CreateTree(BiTree * T)
             exit(1);
         }
         
-        (*T)->n = i;
+        T->n = i;
 
 
    }else
-        (*T)->n= 0;
+        T->n= 0;
 
 }
 
@@ -273,7 +279,7 @@ void Print(BiTree T)
 }
 
 
-void InsertChild(BiTree * T,char p,int i,BiTree c)
+void InsertChild(BiTree T,char p,int i,BiTree c)
 {
    int j;
    int k;
@@ -283,12 +289,12 @@ void InsertChild(BiTree * T,char p,int i,BiTree c)
    
    PtNode temp;
 
-   if(!isTempty(*T))
+   if(!isTempty(T))
    {
         //找到点p所在的结点
-        for(j = 0;j < (*T)->n;j++)
+        for(j = 0;j < T->n;j++)
         { 
-            if(p == (*T)->nodes[j].data)
+            if(p == T->nodes[j].data)
                 break;
         }
         l = j + 1;  //从j + 1的位置上开始插入,当c是p的第一棵子树，从j + 1开始插入
@@ -296,9 +302,9 @@ void InsertChild(BiTree * T,char p,int i,BiTree c)
         //当c不是p的第一棵子树
         if(i > 1)
         {
-            for(k = j + 1;k < (*T)->n;k++)
+            for(k = j + 1;k < T->n;k++)
             {
-                if(j == (*T)->nodes[k].parent)
+                if(j == T->nodes[k].parent)
                 {
                     n++;
                     if(n == i - 1)
@@ -308,46 +314,42 @@ void InsertChild(BiTree * T,char p,int i,BiTree c)
             l = k + 1;    //c实际插入的位置为k + 1
         }
         //将k + 1后的结点依次移动c.n个位置
-        if(l < (*T)->n)
-        {
-            for(k = (*T)->n - 1;k >= l;k--)
+        if(l < T->n)
+            for(k = T->n - 1;k >= l;k--)
             {
-                (*T)->nodes[k + c->n] = (*T)->nodes[k];
-                if((*T)->nodes[k].parent >= l)
-                   (*T)->nodes[k + c->n].parent = (*T)->nodes[k + c->n].parent + c->n;
-                
-                for(k = 0;k < c->n ;k++)
-                {
-                    (*T)->nodes[k + l].data = c->nodes[k].data;
-                    (*T)->nodes[k + l].parent = c->nodes[k].parent + 1;
-                }
+                T->nodes[k + c->n] = T->nodes[k];
+                if(T->nodes[k].parent >= l)
+                   T->nodes[k + c->n].parent = T->nodes[k + c->n].parent + c->n;
+            }   
 
-            }
+        for(k = 0;k < c->n ;k++)
+        {
+            T->nodes[k + l].data = c->nodes[k].data;
+            T->nodes[k + l].parent = c->nodes[k].parent + l;
         }
-        
-        (*T)->nodes[l].parent = j;
-        (*T)->n = (*T)->n + c->n;
+        T->nodes[l].parent = j;
+        T->n = T->n + c->n;
         
         //按照层序排列
         while(f)
         {
             f = 0; //循环标记位
-            for(j = l;j < (*T)->n;j++)
+            for(j = l;j < T->n - 1;j++)
             {
-                if((*T)->nodes[j].parent > (*T)->nodes[j + 1].parent)
+                if(T->nodes[j].parent > T->nodes[j + 1].parent)
                 {
-                    temp = (*T)->nodes[j + 1];
-                    (*T)->nodes[j + 1] = (*T)->nodes[j];
-                    (*T)->nodes[j] = temp;
+                    temp = T->nodes[j];
+                    T->nodes[j] = T->nodes[j + 1];
+                    T->nodes[j + 1] = temp;
                 
                     f = 1;
                 
-                    for(k = j;j < (*T)->n;k++)
+                    for(k = j;k < T->n;k++)
                     {   
-                        if((*T)->nodes[k].parent == j)
-                            (*T)->nodes[k].parent++;
-                        else if((*T)->nodes[k].parent == j + 1)
-                            (*T)->nodes[k].parent--;
+                        if(T->nodes[k].parent == j)
+                            T->nodes[k].parent++;
+                        else if(T->nodes[k].parent == j + 1)
+                            T->nodes[k].parent--;
 
                     }
                 }
@@ -359,4 +361,84 @@ void InsertChild(BiTree * T,char p,int i,BiTree c)
 
 
 
+void DeleteChild(BiTree T,char p,int i)
+{
+    int j;
+    int k;
+    int n = 0;
+    struct queue2 queue;
+    struct Qelement q1;
+    struct Qelement q2;
+   
+    
+    //初始化删除数组值
+    for(j = 0;j < T->n;j++)
+        deleted[j] = 0;
+    
+    //初始化队列
+    init_queue(&queue,100);
+    
+    //找到结点p在树中的序号
+    for(j = 0;j < T->n;j++)
+    {
+        if(T->nodes[j].data == p)
+            break;
+    }
+
+    //找到p结点删除的子数i
+    for(k = j + 1;k < T->n;k++)
+    {   
+        if(T->nodes[k].parent == j)
+            n++;
+        if(n == i)
+            break;
+    }
+
+    //在标记数组deleted中标记所需要删除的结点
+    if(k < T->n)
+    {
+        n = 0;
+        q1.num = k;
+        insert_queue(&queue,q1);
+        deleted[k] = 1;
+        n++;
+        while(!is_empty(&queue))
+        {
+            q2 = delete_queue(&queue);
+            for(j = q2.num + 1; j < T->n;j++)
+            {
+                if(T->nodes[j].parent == q2.num)
+                {
+                    q1.num = j;
+                    insert_queue(&queue,q1);
+                    n++;
+                    deleted[j] = 1;
+                }
+
+            }
+        }
+        
+        //通过删除标记删除对应的结点
+        for(j = 0;j < T->n;j++)
+        {
+            if(deleted[j] == 1)
+            {
+                for(k = j + 1;k < T->n;k++)
+                {   
+                    deleted[k - 1] = deleted[k];
+                    T->nodes[k - 1] = T->nodes[k];
+                    if(T->nodes[k].parent > j)
+                        T->nodes[k - 1].parent--;
+                } 
+
+                j--;
+
+            }
+
+        }
+
+        T->n = T->n - n;
+    }
+
+}
 
